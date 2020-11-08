420704268c9902f5702310d73815fef6d4a371a5
Layinka Jaji <layinkawebdev@gmail.com>
2020-11-08 18:19:51 +0100

    Transfer to bank and transfer to wallet endpoints

--- src/apis/transfers.js
+++ src/apis/transfers.js
@@ -1,22 +1,14 @@
 'use strict'
 
+const sandboxURL = `http://sandbox.cashierapi.operapay.com/api/v3`;
+
 module.exports = {
     /**
      * Transfer to Wallet
      */
     transferToWallet: {
         method: 'post',
-        path: `${process.env.OPAY_URL}/transfer/toWallet`,
-        params: ['merchantid*', 'content-type', 'authorization*'],
-        default_params: {'content-type': 'application/json'},
-    },
-
-    /**
-     * Transfer to Bank
-     */
-    transferToBank: {
-        method: 'post',
-        path: `${process.env.OPAY_URL}/transfer/toBank`,
+        path: `${sandboxURL}/transfer/toWallet`,
         params: ['merchantid*', 'content-type', 'authorization*'],
         default_params: {'content-type': 'application/json'},
     },
@@ -24,9 +16,39 @@ module.exports = {
     /**
      * Wallet Trasfer Status
      */
-    transferStatus: {
+    walletTransferStatus: {
         method: 'post',
-        path: `${process.env.OPAY_URL}/transfer/status/toWallet`,
+        path: `${sandboxURL}/transfer/status/toWallet`,
+        params: ['merchantid*', 'content-type', 'authorization*'],
+        default_params: {'content-type': 'application/json'},
+    },
+
+    /**
+     * Get list of banks
+     */
+    getBanks: {
+        method: 'post',
+        path: `${sandboxURL}/banks`,
+        params: ['merchantid*', 'content-type', 'authorization*'],
+        default_params: {'content-type': 'application/json', countryCode: 'NG'},
+    },
+
+    /**
+     * Transfer to Bank
+     */
+    transferToBank: {
+        method: 'post',
+        path: `${sandboxURL}/transfer/toBank`,
+        params: ['merchantid*', 'content-type', 'authorization*'],
+        default_params: {'content-type': 'application/json'},
+    },
+
+    /**
+     * Bank Transfer status
+     */
+    bankTransferStatus: {
+        method: 'post',
+        path: `${sandboxURL}/transfer/status/toBank`,
         params: ['merchantid*', 'content-type', 'authorization*'],
         default_params: {'content-type': 'application/json'},
     }
