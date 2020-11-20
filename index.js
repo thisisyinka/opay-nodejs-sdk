e48eba94f96af783f4eed6b5f3bc60e4de806335
Layinka Jaji <layinkawebdev@gmail.com>
2020-11-20 15:58:24 +0100

    fix: bloated build size

---
+++ index.js
@@ -0,0 +1,4 @@
+const OPay = require('./src/OPay');
+OPay.prototype.version = '1.0.1';
+
+module.exports = OPay;
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import visualizer from 'rollup-plugin-visualizer';
 import pkg from './package.json';
 
 export default [
   {
-    input: 'src/OPay',
-    plugins: [resolve(), commonjs(), visualizer()],
+    input: './index.js',
+    plugins: [commonjs(), visualizer()],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
